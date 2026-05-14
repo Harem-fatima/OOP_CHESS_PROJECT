1b8a6e851f947f078e6f669e1d1b512db15e7c18
SahrishBatool <sahrishbatool01@gmail.com>
2026-05-14 15:28:02 +0500

    Create Piece.cpp

---
+++ Piece.cpp
@@ -0,0 +1,21 @@
+#include "ChessPieces.h"
+
+// ==========================================
+// Base Class: Piece
+// ==========================================
+Piece::Piece(bool white, char sym)
+{
+    isWhite = white;
+    symbol = sym;
+}
+Piece::~Piece() {}
+
+char Piece::getSymbol()
+{
+    return symbol;
+}
+
+bool Piece::getIsWhite()
+{
+    return isWhite;
+}
